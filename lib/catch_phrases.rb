44f13007941a07d86327c32bfd3436632699f13d
Mai Pham <maitphm@gmail.com>
2020-10-26 22:43:28 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,18 +1,20 @@
 def mario
-  phrase = "It'sa me, Mario!"
+  $phrase = "It'sa me, Mario!"
   puts phrase
 end
 
 def toadstool
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
 
 def link
-  saying = "It's Dangerous To Go Alone! Take This"
+  $saying = "It's Dangerous To Go Alone! Take This"
   puts saying 
 end 
 
 def all_phrases
-  
+  puts phrase 
+  puts status 
+  puts saying
 end 
